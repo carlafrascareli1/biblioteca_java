aa5a352de4904621cf77a86011665a772bb1d149
Carla Frascareli <carlafrascareli@gmail.com>
2018-01-06 16:58:55 -0200

    Campo Autor do livro

--- src/main/java/br/biblioteca/livros/controladores/LivroController.java
+++ src/main/java/br/biblioteca/livros/controladores/LivroController.java
@@ -15,22 +15,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.biblioteca.livros.beans.Autor;
 import br.biblioteca.livros.beans.Livro;
+import br.biblioteca.livros.repository.AutorRepository;
 import br.biblioteca.livros.repository.LivroRepository;
 
 @Controller
 @RequestMapping("/livros")
 public class LivroController {
-	
+
 	private FileSaver fileSaver = new FileSaver();
-	
+
+	@Autowired
+	private AutorRepository autorRepository;
+
 	@Autowired
 	private LivroRepository livroRepository;
+
 	@GetMapping("")
 	public ModelAndView index() {
 		return this.livros();
 	}
-	
+
 	@GetMapping("/list")
 	public ModelAndView livros() {
 
@@ -38,51 +44,71 @@ public class LivroController {
 		return new ModelAndView("livros/list", "livros", livros);
 
 	}
-	
+
 	@GetMapping("/novo")
-	public String createForm(@ModelAttribute Livro livro) {
-		return "livros/form";
-	} 
+	public ModelAndView createForm(@ModelAttribute Livro livro) {
+		ModelAndView modelAndView = new ModelAndView("livros/form");
+
+		Iterable<Autor> autores = autorRepository.findAll();
+		modelAndView.addObject("autores", autores);
+
+		return modelAndView;
+	}
 
 	@PostMapping(params = "form")
-	public ModelAndView create(@ModelAttribute("livro")  @Valid Livro livro, BindingResult bindingResult, Model model
-			, @RequestParam("capaUrl") MultipartFile capaUrl) {
-		
-		if(capaUrl.getOriginalFilename().equals("")) {
-			model.addAttribute("message", "A capa não pode ser vazia");
-			return new ModelAndView("livros/form");
-		}else {
-			if(capaUrl.getContentType().equals("image/jpeg")){
-				String webPath = fileSaver.write("uploaded-images",capaUrl);
-				livro.setCapa(webPath);
-			}else{
-				model.addAttribute("message", "Arquivo em formato errado. Permitido apenas jpg");
-				return new ModelAndView("livros/form");
+	public ModelAndView create(@ModelAttribute("livro") @Valid Livro livro, BindingResult bindingResult, Model model,
+			@RequestParam("capaUrl") MultipartFile capaUrl) {
+
+		if (livro.getId() != null) {
+			if (capaUrl.getOriginalFilename().length() > 0) {
+				if (capaUrl.getContentType().equals("image/jpeg")) {
+					String webPath = fileSaver.write("uploaded-images", capaUrl);
+					livro.setCapa(webPath);
+				} else {
+					model.addAttribute("capa", "Arquivo em formato errado. Permitido apenas jpg");
+				}
+			}
+		} else {
+			if (capaUrl.getOriginalFilename().equals("")) {
+				model.addAttribute("capa", "A capa não pode ser vazia");
+			} else {
+				if (capaUrl.getContentType().equals("image/jpeg")) {
+					String webPath = fileSaver.write("uploaded-images", capaUrl);
+					livro.setCapa(webPath);
+				} else {
+					model.addAttribute("capa", "Arquivo em formato errado. Permitido apenas jpg");
+				}
 			}
 		}
-		
-		if (bindingResult.hasErrors()  || model.containsAttribute("capa")) {
-			return new ModelAndView("livros/form");
-		}
-		
-		livro = this.livroRepository.save(livro);
-	    return new ModelAndView("redirect:/livros/list");
-	} 
 
-	
+		if (bindingResult.hasErrors() || model.containsAttribute("message")) {
+			Iterable<Autor> autores = autorRepository.findAll();
+			return new ModelAndView("livros/form", "autores", autores);
+		}
+
+		livro = this.livroRepository.save(livro);
+		return new ModelAndView("redirect:/livros/list");
+	}
+
 	@GetMapping("/alterar/{id}")
 	public ModelAndView alterar(@PathVariable("id") Long id) {
+
 		Livro livro = this.livroRepository.findOne(id);
-		return new ModelAndView("livros/form", "livro", livro);
+		Iterable<Autor> autores = autorRepository.findAll();
+
+		ModelAndView modelAndView = new ModelAndView("livros/form");
+		modelAndView.addObject("autores", autores);
+		modelAndView.addObject("livro", livro);
+
+		return modelAndView;
+
 	}
-	
+
 	@GetMapping("/excluir/{id}")
 	public ModelAndView excluir(@PathVariable("id") Long id) {
 		Livro livro = this.livroRepository.findOne(id);
 		this.livroRepository.delete(livro);
 		return new ModelAndView("redirect:/livros/list");
 	}
-	
-	
-	
+
 }
