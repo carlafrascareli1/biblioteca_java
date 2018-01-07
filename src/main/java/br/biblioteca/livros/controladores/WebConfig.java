3b773c857641d5f16b2e2217949f5c78ccabba44
Carla Frascareli <carlafrascareli@gmail.com>
2018-01-07 02:13:47 -0200

    Layout

--- src/main/java/br/biblioteca/livros/controladores/WebConfig.java
+++ src/main/java/br/biblioteca/livros/controladores/WebConfig.java
@@ -1,17 +1,20 @@
 package br.biblioteca.livros.controladores;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-  	@Override
-  	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    		registry.addResourceHandler("/uploaded-images/**")
-.addResourceLocations("file:/C:/temp/biblioteca/uploaded-images/");
-  	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/uploaded-images/**")
+				.addResourceLocations("file:/C:/temp/biblioteca/uploaded-images/");
+		
+		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
+	}
 
 }
-
